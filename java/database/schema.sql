25c35016a636d902eaa1c5357608088b4d231688
Jake Elsayed 2 <jelsayed7@outlook.com>
2023-04-10 13:01:12 -0400

    Database Schema Made

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
-BEGIN TRANSACTION;
+start transaction;
 
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users, course, league, league_player, match, match_player, scoretable;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -10,4 +10,62 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-COMMIT TRANSACTION;
+create table course(
+	course_id serial primary key,
+	course_name varchar(50) not null,
+	location_lat varchar(50) not null,
+	location_long varchar(50) not null,
+	address varchar(50) not null,
+	city varchar(50) not null,
+	state varchar(50) not null,
+	country varchar(50) not null
+);
+
+create table league(
+	league_id serial primary key,
+	league_name varchar(50) unique not null,
+	description varchar(200) not null,
+	course_id int not null,
+	admin_id int not null,
+	
+	constraint fk_home_course_id foreign key (course_id) references course (course_id),
+	constraint fk_league_admin_id foreign key (admin_id) references users (user_id)
+);
+
+create table league_player(
+	league_id int not null,
+	player_id int not null,
+	
+	constraint league_id foreign key (league_id) references league (league_id),
+	constraint player_id foreign key (player_id) references users (user_id)
+);
+
+create table match(
+	match_id serial primary key,
+	league_id int not null,
+	start_time timestamp not null,
+	end_time timestamp constraint end_time_past_start check (end_time > start_time) not null,
+	is_completed boolean not null,
+	
+	constraint league_id foreign key (league_id) references league (league_id)
+);
+
+create table match_player(
+	match_id int not null,
+	player_id int not null,
+	
+	constraint match_id foreign key (match_id) references match (match_id),
+	constraint player_id foreign key (player_id) references users (user_id)
+);
+
+create table scoretable(
+	scoretable_id serial primary key,
+	match_id int not null,
+	player_id int not null,
+	score int not null,
+	
+	constraint match_id foreign key (match_id) references match (match_id),
+	constraint player_id foreign key (player_id) references users (user_id)
+);
+
+commit;
