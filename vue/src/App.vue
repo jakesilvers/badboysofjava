05ad8a74b38f3ff7b9ff1e04a62af8cdf6ae9717
Sam Smith <me@smlksmth.com>
2023-04-10 14:13:09 -0400

    login page/create account page

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+    <div id="app">
+        <div id="nav">
+            <!-- <router-link v-bind:to="{ name: 'home' }"
+                ><h4>HOME</h4></router-link
+            >
+            <router-link
+                v-bind:to="{ name: 'logout' }"
+                v-if="$store.state.token != ''"
+                >Logout</router-link
+            > -->
+        </div>
+        <router-view />
     </div>
-    <router-view />
-  </div>
 </template>
+
+<style>
+#nav {
+    text-align: center;
+    margin-top: 30px;
+}
+
+a {
+    color: white;
+}
+</style>
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,71 +1,149 @@
 <template>
-  <div id="login">
-    <form @submit.prevent="login">
-      <h1 >Please Sign In</h1>
-      <div role="alert" v-if="invalidCredentials">
-        Invalid username and password!
-      </div>
-      <div role="alert" v-if="this.$route.query.registration">
-        Thank you for registering, please sign in.
-      </div>
-      <div class="form-input-group">
-        <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
-      </div>
-      <div class="form-input-group">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="user.password" required />
-      </div>
-      <button type="submit">Sign in</button>
-      <p>
-      <router-link :to="{ name: 'register' }">Need an account? Sign up.</router-link></p>
-    </form>
-  </div>
+    <div id="login">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 image-div">
+                    <img
+                        class="d-block"
+                        src="https://i.imgur.com/ffeaW6I.png"
+                        alt=""
+                    />
+                </div>
+                <div class="card login-box shadow p-3 mb-5 bg-white rounded">
+                    <div class="card-body login-form">
+                        <form @submit.prevent="login">
+                            <div>
+                                <h1 class="mb-4">Sign In</h1>
+                            </div>
+                            <div role="alert" v-if="invalidCredentials">
+                                Invalid username and password!
+                            </div>
+                            <div
+                                role="alert"
+                                v-if="this.$route.query.registration"
+                            >
+                                Thank you for registering, please sign in.
+                            </div>
+                            <div class="form-input-group">
+                                <label class="form-control-label" for="username"
+                                    >Username</label
+                                >
+                                <input
+                                    class="form-control"
+                                    type="text"
+                                    id="username"
+                                    v-model="user.username"
+                                    required
+                                    autofocus
+                                />
+                            </div>
+                            <div class="form-input-group">
+                                <label class="form-control-label" for="password"
+                                    >Password</label
+                                >
+                                <input
+                                    class="form-control"
+                                    type="password"
+                                    id="password"
+                                    v-model="user.password"
+                                    required
+                                />
+                            </div>
+                            <div class="login-btn login-text">
+                                <button
+                                    class="btn btn-outline-primary"
+                                    type="submit"
+                                >
+                                    Sign in
+                                </button>
+                            </div>
+                            <p>
+                                <router-link :to="{ name: 'register' }"
+                                    >Need an account? Sign up.</router-link
+                                >
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import authService from "../services/AuthService";
 
 export default {
-  name: "login",
-  components: {},
-  data() {
-    return {
-      user: {
-        username: "",
-        password: ""
-      },
-      invalidCredentials: false
-    };
-  },
-  methods: {
-    login() {
-      authService
-        .login(this.user)
-        .then(response => {
-          if (response.status == 200) {
-            this.$store.commit("SET_AUTH_TOKEN", response.data.token);
-            this.$store.commit("SET_USER", response.data.user);
-            this.$router.push("/");
-          }
-        })
-        .catch(error => {
-          const response = error.response;
+    name: "login",
+    components: {},
+    data() {
+        return {
+            user: {
+                username: "",
+                password: ""
+            },
+            invalidCredentials: false
+        };
+    },
+    methods: {
+        login() {
+            authService
+                .login(this.user)
+                .then((response) => {
+                    if (response.status == 200) {
+                        this.$store.commit(
+                            "SET_AUTH_TOKEN",
+                            response.data.token
+                        );
+                        this.$store.commit("SET_USER", response.data.user);
+                        this.$router.push("/");
+                    }
+                })
+                .catch((error) => {
+                    const response = error.response;
 
-          if (response.status === 401) {
-            this.invalidCredentials = true;
-          }
-        });
+                    if (response.status === 401) {
+                        this.invalidCredentials = true;
+                    }
+                });
+        }
     }
-  }
 };
 </script>
 
-<style scoped>
-.form-input-group {
-  margin-bottom: 1rem;
+<style>
+.login-box {
+    height: auto;
+    text-align: left;
+    margin-left: auto;
+    margin-right: auto;
+    width: 50%;
+    max-width: 400px;
 }
-label {
-  margin-right: 0.5rem;
+
+html,
+body {
+    height: 100%;
+    background: linear-gradient(0deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
+        url("https://images.unsplash.com/photo-1538423070486-9292881ea10a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80");
+
+    background-size: cover;
+}
+
+p {
+    margin-top: 20px;
+}
+
+.btn {
+    margin-top: 20px;
+}
+
+.image-div {
+    display: flex;
+    justify-content: center;
+}
+
+img {
+    width: 300px;
 }
 </style>
