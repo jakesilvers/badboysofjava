5d00939b2c806984ee5d6447b6fef2352cd84605
Jake Silvers <steaksilvers@yahoo.com>
2023-04-14 14:09:51 -0400

    scorecard dao and CONTROLLA

--- java/src/main/java/com/techelevator/dao/JdbcScoreCardDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcScoreCardDao.java
@@ -1,4 +1,109 @@
 package com.techelevator.dao;
 
-public class JdbcScoreCardDao {
+import com.techelevator.model.ScoreCard;
+import com.techelevator.model.User;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Component;
+
+import java.security.Principal;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Component
+public class JdbcScoreCardDao implements ScoreCardDao {
+
+    private JdbcTemplate jdbcTemplate;
+    private UserDao userDao;
+
+    public JdbcScoreCardDao(JdbcTemplate jdbcTemplate, UserDao userDao) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.userDao = userDao;
+    }
+
+    @Override
+    public int createScoreCard(ScoreCard s) {
+        int scorecardId;
+
+        String sql = "INSERT INTO scorecard (match_id, player_id, score) VALUES (?, ?, ?) RETURNING scoretable_id";
+        try {
+            scorecardId = jdbcTemplate.queryForObject(sql, Integer.class, s.getMatchID(), s.getPlayerID(), s.getScoreValue());
+        } catch (NullPointerException e) {
+            throw new NullPointerException("Unable to create Scorecard");
+
+        }
+        return scorecardId;
+    }
+
+    @Override
+    public int getAverage(int userId) {
+        String sql = "SELECT AVG(score) AS avg_score FROM scorecard JOIN users ON scorecard.player_id = users.user_id WHERE users.user_id = ?";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql, userId);
+        if (result.next()) {
+            return result.getInt(1);
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public List<ScoreCard> showAllScoreCardsForUser(int userId) {
+        String sql = "SELECT * FROM scorecard JOIN league_player ON scorecard.player_id = players.player_id WHERE players.user_id = ?";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, userId);
+        List<ScoreCard> scoreCards = new ArrayList<>();
+        while (results.next()) {
+            ScoreCard s = mapRowToScoreCard(results);
+            scoreCards.add(s);
+        }
+        return scoreCards;
+    }
+
+
+
+    @Override
+    public ScoreCard getScoreCardID(int scoreCardID) {
+        String sql = "SELECT * FROM scorecard WHERE scoretable_id = ?";
+        SqlRowSet result = jdbcTemplate.queryForRowSet(sql, scoreCardID);
+        if (result.next()) {
+            return mapRowToScoreCard(result);
+        } else {
+            return null;
+        }
+    }
+
+    public List<ScoreCard> getScoreCardsByMatchID(int matchID) {
+
+        List<ScoreCard> matchScoreCards = new ArrayList<>();
+        String sql = "SELECT * FROM scorecard WHERE match_id = ?; ";
+        SqlRowSet results = jdbcTemplate.queryForRowSet(sql, matchID);
+
+        while(results.next()) {
+            ScoreCard s = mapRowToScoreCard(results);
+            matchScoreCards.add(s);
+        }
+        if (matchScoreCards.size() > 0) {
+            return matchScoreCards;
+        }
+        return null;
+
+
+    }
+
+
+
+    private ScoreCard mapRowToScoreCard (SqlRowSet rs) {
+        ScoreCard s = new ScoreCard();
+
+        s.setScoreCardID(rs.getInt("scoretable_id"));
+        s.setMatchID(rs.getInt("match_id"));
+        s.setPlayerID(rs.getInt("player_id"));
+        s.setScoreValue(rs.getInt("score"));
+        return s;
+
+    }
+
+
+
 }
